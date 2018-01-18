780fbd8f03405feb6a44f98cc9c74015bd59154a
tangzhanshuai <451384507@qq.com>
2018-01-18 17:13:49 +0800

    tang

--- src/main.js
+++ src/main.js
@@ -1,40 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
-import axios from 'axios'
+import YDUI from 'vue-ydui'
 
 import '@/assets/css/main.css'
 
 Vue.use(Vuex);
+Vue.use(YDUI);
 Vue.config.productionTip = false
 
 const store = new Vuex.Store({
 	state:{
-		nickName:'',
-		cartCount:0
-	},
-	getters:{
-		nickName(state){
-			return state.nickName + ',hello';
-		}
+		nickName:''
 	},
 	mutations:{
 		updateName(state,nickName){
 			state.nickName = nickName;
-		},
-		updateCartCount(state,num){
-			state.cartCount += num;
-		}
-	},
-	actions:{
-		updateNameAction(context,nickName){
-			context.commit('updateName',nickName);
-		},
-		updateCounterAction(context,num){
-			context.commit('updateCartCount',num);
 		}
 	}
 })
--- src/views/Submit.vue
+++ src/views/Submit.vue
@@ -2,234 +2,220 @@
   <div class="gray-wrapper">
     <header class="header white-bg">
       <h1>销售帐薄</h1>
-      <img class="add-people" src="/static/images/add-people.png" @click="addModal">
     </header>
 
     <div class="content">
       <!-- 订单详情 -->
       <div class="white-bg list-item">
         <div class="item-top">
-          <p class="order-num">订单编号：2345678900987654321</p>
-          <p>A打包站百合打包站</p>
-          <p><img src="/static/images/enter-icon.png" alt="">纸厂D</p>
+          <p class="order-num">订单编号：{{info.transMap.order_num}}</p>
+          <p>打包站名称：{{info.transMap.package_site_name}}</p>
+          <p><img src="/static/images/enter-icon.png" /> {{info.transMap.recycle_factory_name}}</p>
         </div>
         <div class="item-bottom">
           <p>
             <label>司机姓名：</label>
-            <span>张得</span>
+            <span>{{info.transMap.carrier_name}}</span>
           </p>
           <p>
             <label>司机手机号：</label>
-            <span>13245454545</span>
+            <span>{{info.transMap.carrier_mobile}}</span>
           </p>
           <p>
-            <label>车牌号：</label>
-            <span>京A 12345</span>
+            <label>发运车牌号：</label>
+            <span>{{info.transMap.carrier_carnum}}</span>
           </p>
         </div>
         <div class="date-box">
-          <label>到场日期：</label>
-          <input type="text" class="date" placeholder="请选择日期" readonly />
+          <label>到厂日期：</label>
+            <span>{{info.transMap.addirmTime}}</span>
         </div>
       </div>
 
       <!-- 回收品类 -->
-      <div class="white-bg recycle-box">
+      <div class="white-bg recycle-box" v-for="item in info.transDetailList">
         <div class="recycle-title">
-          回收品类：A类
+          回收品类：{{item.recycleTypeTitle}}
         </div>
         <div class="recycle-content">
           <div class="recycle-item">
             <div>
-              <label>毛重：</label>
-              <input type="text" value="10T">
-            </div>
-            <div>
-              <label>到厂毛重：</label>
-              <input type="text" value="10T">
-            </div>
-          </div>
-          <div class="recycle-item">
-            <div>
-              <label>皮重：</label>
-              <input type="text" value="2T">
-            </div>
-            <div>
-              <label>到厂皮重：</label>
-              <input type="text" value="2T">
-            </div>
-          </div>
-          <div class="recycle-item">
-            <div>
-              <label>净重：</label>
-              <input type="text" value="8T">
+              <label>出站净重：</label>
+              <span>{{item.transportWeight/1000000}}T</span>
             </div>
             <div>
               <label>到厂净重：</label>
-              <input type="text" value="8T">
+              <span>{{item.affirmWeight/1000000}}T</span>
             </div>
           </div>
           <div class="recycle-item">
             <div>
               <label>发运包数：</label>
-              <input type="text" value="8个">
+              <span>{{item.packageNum}}个</span>
             </div>
             <div>
               <label>到厂包数：</label>
-              <input type="text" value="8个">
+              <span>{{item.affirmPackageNum}}个</span>
             </div>
           </div>
           <div class="recycle-item">
             <div>
               <label>结算净重：</label>
-              <input type="text" value="2T">
+              <span>{{item.affirmWeight/1000000}}T</span>
             </div>
             <div>
               <label>结算单价：</label>
-              <input type="text" value="2T">
+              <span>{{item.unitPrice/100}}元</span>
             </div>
           </div>
           <div class="recycle-item">
             <div>
-              <label>应收账款：</label>
-              <input type="text" value="25">
-            </div>
-          </div>
-          <div class="recycle-item">
-            <div>
-              <label>扣杂：</label>
-              <input type="text" value="8个">
-            </div>
-            <div>
-              <label>扣水：</label>
-              <input type="text" value="8个">
+              <label>分类应收：</label>
+              <span>{{item.totalPrice/100}}元</span>
             </div>
           </div>
         </div>
       </div>
 
-      <!-- 回收品类 -->
-      <div class="white-bg recycle-box">
-        <div class="recycle-title">
-          回收品类：B类
-        </div>
+    </div>
+
+    <div class="white-bg recycle-box">
         <div class="recycle-content">
           <div class="recycle-item">
             <div>
-              <label>毛重：</label>
-              <input type="text" value="10T">
+              <label>扣杂：</label>
+              <span>{{info.transMap.dross_percent/1000000}}T</span>
             </div>
             <div>
-              <label>到厂毛重：</label>
-              <input type="text" value="10T">
+              <label>扣税：</label>
+              <span>{{info.transMap.water_percent/1000000}}T</span>
             </div>
           </div>
           <div class="recycle-item">
+            <div>
+              <label>运费：</label>
+              <span>{{info.transMap.car_money/100}}元</span>
+            </div>
             <div>
               <label>皮重：</label>
-              <input type="text" value="2T">
+              <span>{{info.transMap.tare_weight/1000000}}T</span>
             </div>
+          </div>
+          <div class="recycle-item">
             <div>
               <label>到厂皮重：</label>
-              <input type="text" value="2T">
+              <span>{{info.transMap.affirmTare/1000000}}T</span>
+            </div>
+            <div>
+              <label>毛重：</label>
+              <span>{{info.transMap.gross_weight/1000000}}T</span>
             </div>
           </div>
           <div class="recycle-item">
             <div>
-              <label>净重：</label>
-              <input type="text" value="8T">
+              <label>到厂毛重：</label>
+              <span>{{info.transMap.affirmGross/1000000}}T</span>
             </div>
             <div>
-              <label>到厂净重：</label>
-              <input type="text" value="8T">
-            </div>
-          </div>
-          <div class="recycle-item">
-            <div>
-              <label>发运包数：</label>
-              <input type="text" value="8个">
-            </div>
-            <div>
-              <label>到厂包数：</label>
-              <input type="text" value="8个">
-            </div>
-          </div>
-          <div class="recycle-item">
-            <div>
-              <label>结算净重：</label>
-              <input type="text" value="2T">
-            </div>
-            <div>
-              <label>结算单价：</label>
-              <input type="text" value="2T">
-            </div>
-          </div>
-          <div class="recycle-item">
-            <div>
-              <label>应收账款：</label>
-              <input type="text" value="25">
-            </div>
-          </div>
-          <div class="recycle-item">
-            <div>
-              <label>扣杂：</label>
-              <input type="text" value="8个">
-            </div>
-            <div>
-              <label>扣水：</label>
-              <input type="text" value="8个">
+              <label>应收总款：</label>
+              <span>{{info.transMap.return_money/100}}元</span>
             </div>
           </div>
         </div>
-      </div>
     </div>
 
     <div class="white-bg sub-bottom">
       <p>备注：</p>
       <div class="ps">
-        <textarea placeholder="请填写备注"></textarea>
+        <textarea placeholder="请填写备注" v-model="info.transMap.comment"></textarea>
       </div>
       <p>纸厂回执：</p>
       <div class="camera-box">
-        <img src="/static/images/camera2.png" alt="">
-        <input type="file" class="file">
+        <img class="img" :src="info.transMap.imagePath" alt="图片">
       </div>
     </div>
 
     <div class="btn-group">
-      <span class="edit">修改</span>
-      <span class="submit">提交</span>
+      <span class="submit" @click="submitFn">提交</span>
     </div>
 
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+  import { Confirm, Alert, Toast, Notify, Loading } from 'vue-ydui/dist/lib.rem/dialog';
   export default {
     data () {
       return {
-        overlayFlag:false,
-        mdShowFlag:false,
+        info: null
       }
+    },
+    mounted(){
+      // this.$dialog.loading.open('很快加载好了');
+      // Confirm({});
+      // Alert({});
+      // Toast({});
+      // Notify({});
+      // Loading({});
+
+      //获取订单详情
+      this.getInfo();
     },
     components:{
       
     },
     methods:{
-      //新增客户模态框
-      addModal(){
-        this.mdShowFlag = true;
-        this.overlayFlag = true;
+      //获取订单详情
+      getInfo(){
+        let orderNum = this.$route.query.orderNum;
+        axios.get('/transreceipt/selectorderinfo',{
+          params:{
+            orderNum: orderNum
+          }
+        }).then((result) => {
+          console.log(result);
+          this.info = result.data;
+        }).catch((error)=>{
+          console.log(error);
+        })
       },
-      //关闭模态框
-      closeModal(){
-        this.mdShowFlag = false;
-        this.overlayFlag = false;
-      },
-      //关闭遮罩
-      closePop(){
-        this.mdShowFlag = false;
-        this.overlayFlag = false;
+      //提交
+      submitFn(){
+        let detailData = [];
+        this.info.transDetailList.map((item,index) =>{
+          detailData.push(item.recycleTypeId + ':' + (item.affirmWeight?item.affirmWeight:0) + ':' + (item.packageNum?item.packageNum:0) + ':' + (item.unitPrice?item.unitPrice:0) + ':' + (item.totalPrice?item.totalPrice:0));
+        })
+
+        var params = new URLSearchParams();
+        params.append('orderNum', this.info.transMap.order_num || 0);
+        params.append('grossWeight', this.info.transMap.gross_weight || 0);
+        params.append('tareWeight', this.info.transMap.tare_weight || 0);
+        params.append('detail', detailData.join(',') || '');
+        params.append('waterPercent', this.info.transMap.water_percent || 0);
+        params.append('drossPercent', this.info.transMap.dross_percent || 0);
+        params.append('carMoney', this.info.transMap.car_money || 0);
+        params.append('imagePath', this.info.transMap.image_path || '');
+        params.append('comment', this.info.transMap.comment);
+
+        axios.post('/transreceipt/insertreceipt', params).then((result)=>{
+          console.log(result);
+          if(result.data.code == 0){
+            Toast({
+                mes: '提交成功！',
+                timeout: 1500,
+                icon: 'success'
+            });
+          }else{
+            Toast({
+                mes: result.data.msg,
+                timeout: 1500,
+                icon: 'error'
+            });
+          }
+        }).catch((error) => {
+          console.log(error);
+        })
       }
     }
   }
@@ -316,14 +302,13 @@
           label{
 
           }
-          input{
+          span{
+            display: inline-block;
             width: 15rem;
             height: 4rem;
             line-height: 4rem;
             text-indent: 5px;
-            color: #666;
-            border: 1px solid #ddd;
-            border-radius: 5px;
+            color: #999;
           }
         }
       }
@@ -351,18 +336,13 @@
       width: 100%;
       height: 15rem;
       position: relative;
-      img{
-        width: 100%;
-        height: 100%;
-      }
-      .file{
+      .img{
         width: 100%;
         height: 100%;
         position: absolute;
         left: 0;
         top: 0;
-        z-index: 2;
-        opacity: 0;
+        z-index: 1;
       }
     }
   }
@@ -370,118 +350,12 @@
   .btn-group{
     height: 9.8rem;
     line-height: 9.8rem;
-    display: flex;
-    span{
-      flex: 1;
-      display: block;
-      text-align: center;
-      font-size: 3.6rem;
-      background: #fff;
-    }
-    .edit{
-
-    }
     .submit{
-      background: #00877c;
-      color: #fff;
-    }
-  }
-
-  /*模态框样式*/
-  .my-modal {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    width: 90%;
-    height: auto;
-    margin: 0 auto;
-    padding: 2rem;
-    background: #fff;
-    -webkit-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    z-index: 900;
-  }
-
-  .md-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.4);
-    z-index: 899;
-  }
-  .md-modal-transition.md-show {
-    visibility: visible;
-  }
-
-  .modal-top{
-    display: flex;
-    margin-bottom: 2rem;
-    .back{
-      flex: 0 0 1.4rem;
-      width: 1.4rem;
-      height: 2.6rem;
-    }
-    h1{
-      font-size: 3.4rem;
-      color: #333;
-      font-weight: normal;
-      flex: 1;
-      text-align: center;
-    }
-  }
-  .modal-body{
-    .form-group{
-      line-height: 6rem;
-      display: flex;
-      margin-bottom: 2rem;
-      label{
-        flex: 0 0 15rem;
-      }
-      .input-box{
-        flex: 1;
-        input{
-          width: 86%;
-          height: 6rem;
-          line-height: 6rem;
-          border: 1px solid #ddd;
-          font-size: 3rem;
-          text-indent: .5rem;
-        }
-        .textarea{
-          width: 86%;
-          border: 1px solid #ddd;
-          font-size: 3rem;
-          text-indent: .5rem;
-        }
-      }
-      .linkman{
-        position: relative;
-        .album{
-          width: 4rem;
-          height: 4.4rem;
-          position: absolute;
-          right: 0;
-          top: 1rem;
-        }
-      }
-    }
-  }
-  .modal-bottom{
-    margin: 5rem 0;
-    .save-btn{
       display: block;
-      width: 80%;
-      height: 8rem;
-      line-height: 8rem;
-      margin: 0 auto;
       text-align: center;
       font-size: 3.6rem;
       background: #00877c;
       color: #fff;
-      border-radius: 4rem;
     }
   }
 </style>
